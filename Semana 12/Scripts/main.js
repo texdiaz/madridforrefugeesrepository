17ef85157f8b68ac1363a17a709043663c4960af
texdiaz <texdiaz@gmail.com>
2019-06-06 19:54:23 +0200

    los deberes actualizados

---
+++ Semana 12/Scripts/main.js
@@ -0,0 +1,48 @@
+
+var pregunta = " cual es tu nombre?"
+var nombre1 =prompt(pregunta);
+var pregunta2 = " cuantos años de edad tienes?"
+var edad1 =prompt(pregunta2);
+var pregunta3 = " cual es tu nombre?"
+var nombre2 =prompt(pregunta3);
+var pregunta4 = " cuantos años de edad tienes?"
+var edad2 =prompt(pregunta4);
+var pregunta5 = " cual es tu nombre?"
+var nombre3 =prompt(pregunta5);
+var pregunta6 = " cuantos años de edad tienes?"
+var edad3 =prompt(pregunta6);
+var pregunta7 = " cual es tu nombre?"
+var nombre4 =prompt(pregunta7);
+var pregunta8 = " cuantos años de edad tienes?"
+var edad4 =prompt(pregunta8);
+var pregunta9 = " cual es tu nombre?"
+var nombre5 =prompt(pregunta9);
+var pregunta10 = " cuantos años de edad tienes?"
+var edad5 =prompt(pregunta10);
+
+
+alert("el alumno "+ nombre1 + " tiene " + edad1 + " años" );
+
+alert("el alumno "+ nombre2 + " tiene " + edad2 + " años" );
+
+alert("el alumno "+ nombre3 + " tiene " + edad3 + " años" );
+
+alert("el alumno "+ nombre4 + " tiene " + edad4 + " años" );
+
+alert("el alumno "+ nombre5 + " tiene " + edad5 + " años" );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
