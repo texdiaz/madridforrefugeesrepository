bec17e474dc27b507e0d47a98987de72908b80bf
texdiaz <texdiaz@gmail.com>
2019-06-17 21:58:37 +0200

    todos los debres actualizdos

--- Semana 13/Scripts/main.js
+++ Semana 13/Scripts/main.js
@@ -32,12 +32,13 @@ if (num % 3 === 0 && num % 5 === 0){
 }
 
 for (i=0; i<=100; i++){
-    if (i % 3 === 0) {
-        console.log ("Fizz");
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz");
     } else if (i % 5 === 0) {
         console.log ("Buzz");
-    } else if (i % 3 === 0 && i % 5 === 0){
-        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log ("Fizz");
+
     } else {
         console.log(i);
     }  
