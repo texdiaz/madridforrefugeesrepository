4579c735947727a6b4d86ab047fd1a4dc85ce3e1
texdiaz <texdiaz@gmail.com>
2019-06-17 10:40:49 +0200

    los deberes actualizados

--- Semana 13/Scripts/main.js
+++ Semana 13/Scripts/main.js
@@ -33,11 +33,11 @@ if (num % 3 === 0 && num % 5 === 0){
 
 for (i=0; i<=100; i++){
     if (i % 3 === 0) {
-        console.log ('Fizz!');
+        console.log ("Fizz");
     } else if (i % 5 === 0) {
-        console.log ('Buzz!');
+        console.log ("Buzz");
     } else if (i % 3 === 0 && i % 5 === 0){
-        console.log('FizzBuzz!');
+        console.log("FizzBuzz");
     } else {
         console.log(i);
     }  
